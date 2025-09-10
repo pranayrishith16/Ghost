b11bd4432849da6fe3c309ecf614eb14694dc5c0
pranay rishith <52948338+pranayrishith16@users.noreply.github.com>
2025-09-09 22:39:35 -0500

    yes

--- test.py
+++
@@ -1,8 +0,0 @@
-import weaviate
-from src.embedder.sentence_transformer_embedder import SentenceTransformersEmbedder
-
-embedder = SentenceTransformersEmbedder()
-
-client = weaviate.connect_to_local()
-client.collections.delete_all()
-client.close()
